93bc6d24ebd8b3d35289feefc6d5f9cef4b20061
ekscentrysytet <ekscentrysytet@gmail.com>
2017-04-20 17:12:31 +0300

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBoL2b9S5NUGYzhLJYwOn7CTSohq9Zj-8M',
+  authDomain: 'ngfireslack-b614a.firebaseapp.com',
+  databaseURL: 'https://ngfireslack-b614a.firebaseio.com',
+  projectId: 'ngfireslack-b614a',
+  storageBucket: 'ngfireslack-b614a.appspot.com',
+  messagingSenderId: '656122539815'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +22,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
